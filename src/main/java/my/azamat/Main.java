4618476d6e09166549fb1514e9f71e76c7cf4f87
Azamat Ismagilov <azoma06@mail.ru>
2020-02-23 00:48:21 +0500

    add recursion method for showing directories

--- src/main/java/my/azamat/Main.java
+++ src/main/java/my/azamat/Main.java
@@ -5,47 +5,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    static File targetFile = new File("data/fileforwrite.txt");
+
+    static StringBuffer tab = new StringBuffer("");
+    static File fileToWrite = new File("data/fileforwrite.txt");
+
+    public static void showInside(File file) {
+        if (file.isDirectory()) {
+            writeToFile(tab + file.getName());
+            tab.append("    ");
+            for (File current : file.listFiles()) {
+                showInside(current);
+            }
+            writeToFile("");
+            tab.delete(0,4);
+        } else {
+            writeToFile(tab + file.getName());
+        }
+    }
+
+    public static void writeToFile(String file) {
+        try (FileWriter fileWriter = new FileWriter(fileToWrite, true); BufferedWriter out = new BufferedWriter(fileWriter)) {
+            if (file.equals("")) {
+                out.newLine();
+            } else {
+                out.write(file);
+                out.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        targetFile.delete();
+        if (fileToWrite.exists()) {
+            fileToWrite.delete();
+        }
         File fileOrDir = new File(args[0]);
         if (fileOrDir.exists() && fileOrDir.isDirectory()) {
             //"D:\music\ROCK\КИНО\Кино"
-            writeToFile(fileOrDir.getName());
-            int counter = 1;
-            for (File current : fileOrDir.listFiles()) {
-                writeToFile("   " + current.getName());
-                if (current.isDirectory()) {
-                    for (File subCurrent : current.listFiles()) {
-                        if (counter < 10) {
-                            writeToFile("   0" + counter + " - " + subCurrent.getName());
-                        } else {
-                            writeToFile("   " + counter + " - " + subCurrent.getName());
-                        }
-                        counter++;
-                    }
-                    writeToFile("");
-                }
-                counter = 1;
-            }
+            showInside(fileOrDir);
         }
 
-        if (fileOrDir.exists() && fileOrDir.isFile()) {////"data\fileforread.txt"
-            int countOfFolders = 0;
-            int countOfFiles = 0;
-            double sumLengthOfFileNames = 0;
-            Map<Integer, Integer> filesInEachFolder = new HashMap<>();
+        if (fileOrDir.exists() && fileOrDir.isFile()) {//"data\fileforread.txt"
+            int countOfAllFolders = 0;
+            int countOfAllFiles = 0;
+            int currentNumberOfFolder = 0;
+            int countOfFilesInCurrentFolder = 0;
+            double sumOfLengthsOfFileNames = 0;
+            Map<Integer, Integer> filesInFolders = new HashMap<>();
             try (FileReader fileReader = new FileReader(fileOrDir); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
                 String line = null;
                 while ((line = bufferedReader.readLine()) != null) {
                     if (line.contains(".")) {
-                        countOfFiles++;
-                        sumLengthOfFileNames += line.length();
+                        countOfFilesInCurrentFolder++;
+                        countOfAllFiles++;
+                        sumOfLengthsOfFileNames += line.length();
                     } else if (!line.isEmpty()) {
-                        countOfFolders++;
-                    } else {
-                        filesInEachFolder.put(countOfFolders, countOfFiles);
-                        countOfFiles = 0;
+                        countOfAllFolders++;
+                        currentNumberOfFolder++;
+                    } else if (line.isEmpty() && countOfFilesInCurrentFolder != 0){
+                        filesInFolders.put(currentNumberOfFolder, countOfFilesInCurrentFolder);
+                        countOfFilesInCurrentFolder = 0;
                     }
                 }
             } catch (FileNotFoundException e) {
@@ -53,32 +75,11 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            int sumOfFiles = 0;
-            for (Integer value : filesInEachFolder.values()) {
-                sumOfFiles += value;
-            }
-            System.out.println("countOfFolders = " + filesInEachFolder.size());
-            System.out.println("countOfFiles = " + sumOfFiles);
-            System.out.println("avgerage count of files in folders = " + sumOfFiles/filesInEachFolder.size());
-            System.out.println("avgerage length of filenames = " + sumLengthOfFileNames/sumOfFiles);
-            System.out.println(filesInEachFolder);
-        }
 
-
-
-    }
-
-    public static void writeToFile(String file) {
-        try (FileWriter fileWriter = new FileWriter(targetFile, true); BufferedWriter out = new BufferedWriter(fileWriter)) {
-            if (file.equals("")) {
-                out.newLine();
-            } else {
-                out.write(file + "\n");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("countOfAllFolders = " + countOfAllFolders);
+            System.out.println("countOfAllFiles = " + countOfAllFiles);
+            System.out.println("avgerage count of files in folders = " + countOfAllFiles/filesInFolders.size());
+            System.out.println("avgerage length of filenames = " + Math.floor(sumOfLengthsOfFileNames/countOfAllFiles*1e1)/1e1);
         }
     }
 }
